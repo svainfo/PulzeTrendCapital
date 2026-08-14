ebf2bcd8de7400ce9f7e0555f411aaa3d44ac57a
svainfo <svainfo@github.com>
2026-08-14 08:46:46 +0530

    content: update map coordinates to Sotheby Building, Rodney Bay, Saint Lucia

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,7 +14,7 @@ export const SITE_CONFIG = {
   founded: "2026",
   regulator: "Regulated Broker | License No. FRB-XXXXX",
   mapEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.0!2d-74.0115!3d40.7075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQyJzI3LjAiTiA3NMKwMDAnNDIuMCJX!5e0!3m2!1sen!2sus!4v1234567890",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3815.2!2d-60.95563!3d14.07044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTTCsDA0JzEzLjYiTiA2MMKwNTcnMjAuMyJX!5e0!3m2!1sen!2slc!4v1699000000000!5m2!1sen!2slc",
 } as const;
 
 export const NAV_LINKS = [
