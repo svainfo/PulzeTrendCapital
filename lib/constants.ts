87c5e1a622bdbc07099b9ab330304d7b331683da
svainfo <svainfo@github.com>
2026-08-14 09:04:06 +0530

    Map coordinate updates

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,7 +14,7 @@ export const SITE_CONFIG = {
   founded: "2026",
   regulator: "Regulated Broker | License No. FRB-XXXXX",
   mapEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3815.2!2d-60.95563!3d14.07044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTTCsDA0JzEzLjYiTiA2MMKwNTcnMjAuMyJX!5e0!3m2!1sen!2slc!4v1699000000000!5m2!1sen!2slc",
+    "https://maps.google.com/maps?q=14.07044,-60.95563&z=16&output=embed",
 } as const;
 
 export const NAV_LINKS = [
